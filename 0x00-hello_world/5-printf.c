8e2d12dbb0ea250429497c704ffc5ef232033fb0
adhamo0o <adhamelmesady414@gmail.com>
2023-03-16 19:23:15 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
-#include <studio.h>
-int main{void}
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always returns (0)
+ */
+
+int main(void)
 {
-  ptintf{"\"with proper grammar, but the outcome is a piece of art,"}
-  return{0}
+         ptintf{"with proper grammar, but the outcome is a piece of art,"}:
+	 return{0}:
 }
