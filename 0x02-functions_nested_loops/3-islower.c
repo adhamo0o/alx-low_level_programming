02b79ec432aa22a0526dac7e076912c992381d0b
adhamo0o <adhamelmesady414@gmail.com>
2023-03-22 12:45:15 -0700

    a

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_islower(int c)
+ *Return:  if true
+ *Return: 0 if false
+ */
+int _islower(int c)
+{
+if (c <= 122 && c >= 97)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putcar ('\n');
+}
