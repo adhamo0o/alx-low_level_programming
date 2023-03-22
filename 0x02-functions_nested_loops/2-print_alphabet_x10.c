7200354e5a513951c0bfeaaf99a171edad992b6c
adhamo0o <adhamelmesady414@gmail.com>
2023-03-21 22:52:33 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 {
 char x;
 int y;
-for(y = 0 ; y <= 9 ; y++)
+for (y = 0 ; y <= 9 ; y++)
 {
-for(x ='a'; x <= 'z' ; x++)
+for (x = 'a'; x <= 'z' ; x++)
 {
  _putchar(x);
 }
