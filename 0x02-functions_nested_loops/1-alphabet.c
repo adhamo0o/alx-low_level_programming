9b7351bd9388d04232c6b8cd4a8da41c16f5972f
adhamo0o <adhamelmesady414@gmail.com>
2023-03-21 18:42:41 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,7 +4,14 @@
  * print_alphabet - prints alphabet in lowercase
  * Return: 0
  */
+
 int _putchar(char c);
+void print_alphabet(void);
+
+int main() {
+  print_alphabet();
+  return 0;
+}
 void print_alphabet(void)
 {
 	char alpha;
