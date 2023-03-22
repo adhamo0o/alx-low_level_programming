731105e1d09e0c192e49b39dd80da707f63d9199
adhamo0o <adhamelmesady414@gmail.com>
2023-03-21 18:56:34 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,19 @@
-#include <stdio.h>
+#include "holberton.h"
 
-void print_alphabet(void) {
-    char letter = 'a';
-    while (letter <= 'z') {
-        _putchar(letter);
-        letter++;
-    }
-    _putchar('\n');
+/**
+ * print_alphabet - A function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 }
