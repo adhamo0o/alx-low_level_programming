374e1b1b54d6c2acbe2bb3e0a3c5b65dda658d14
adhamo0o <adhamelmesady414@gmail.com>
2023-03-22 13:24:56 -0700

    a

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,13 @@
 #include "main.h"
+
 /**
- *_isalpha - is a funcation test if a letter is lower or ypper case
- *@c: - char
- *Return: 0 (Seccess)
- */
+  * _isalpha - Shows 1 if the input is a
+  * letter Another cases, shows 0
+  *
+  * @c: The character in ASCII code
+  *
+  * Return: 1 for letters. 0 for the rest.
+  */
 int _isalpha(int c)
 {
 if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
