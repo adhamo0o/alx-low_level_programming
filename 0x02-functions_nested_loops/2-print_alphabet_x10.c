14e455463dbdbecd67c3ee50fcba1cbafd311517
adhamo0o <adhamelmesady414@gmail.com>
2023-03-21 22:26:59 -0700

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphanet - prints out the alphabet 
+ *Return: 0 if succesgul
+ */
+void print_alpnabet(void)
+{
+  char x;
+  int y;
+  for(y=0;y<=10;y++)
+    {
+      for(x='a';x<='z';x++)
+	{
+	  _putchar(x);
+	}
+      _putchar('\n');
+    }
+}
