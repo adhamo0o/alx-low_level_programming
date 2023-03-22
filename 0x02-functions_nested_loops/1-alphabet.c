93024b9230f887b06e1b8f854fa7fb0e7eb30883
adhamo0o <adhamelmesady414@gmail.com>
2023-03-21 19:02:53 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,10 @@
-#include main.h"
+#include <stdio.h>
 
-/**
- * print_alphabet - A function that prints the alphabet,
- * in lowercase, followed by a new line.
- *
- * Return: void
- */
-void print_alphabet(void)
-{
-	char c = 'a';
+void print_alphabet(void) {
+    char c;
 
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-	}
-	_putchar('\n');
+    for (c = 'a'; c <= 'z'; c++) {
+        _putchar(c);
+    }
+    _putchar('\n');
 }
