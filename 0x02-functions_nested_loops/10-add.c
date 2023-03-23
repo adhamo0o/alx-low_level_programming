5692abac5ca571937cce180e6f1a71ac63893581
adhamo0o <adhamelmesady414@gmail.com>
2023-03-22 17:44:35 -0700

    a

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- *add - fancuation that add two integers and returns the result
+ *add - fancuation that add two integers and 
+ *returns the result
  *@a: number one
  *@b: nember two
  */
 int add(int a, int b)
 {
-  return (a + b);
+return (a + b);
 }
