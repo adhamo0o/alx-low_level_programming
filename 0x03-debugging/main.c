db89ac463348fe5abe6b4bf4cae21c15860f64c0
adhamo0o <adhamelmesady414@gmail.com>
2023-03-22 21:48:49 -0700

    a

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,12 @@
+include "main.h"
+/**
+ *main - test function that prints if integer is posotive or negative
+ *Return: 0
+ */
+int main(void)
+{
+int i;
+i = 0;
+posotive_or_negative(1);
+return (0);
+}
