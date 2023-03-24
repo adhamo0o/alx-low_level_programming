6b1c2cce87e48f2756539e2ec6fd4a0f1b212588
adhamo0o <adhamelmesady414@gmail.com>
2023-03-23 23:18:42 -0700

    a

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *
+ *
+ */
+void print_diagonal(int n)
+{
+  int x;
+  int y;
+  if (n <= 0)
+    {
+    _putchar('\n')
+      }
+    else
+      {
+	for (x = 0; x < n; x++)
+	  {
+	    for (y = 0; y < n; y++)
+	      {
+		if(j == i)
+		_putcher('\\');
+	      else if (j < i)
+		_putcher (' ');
+	      }
+	    _putchar('\n');
+	  }
+      }
+}
+		  
