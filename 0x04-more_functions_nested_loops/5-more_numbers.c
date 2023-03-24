ecda5be50750c3d66a6d0d0304efcc06a40d8454
adhamo0o <adhamelmesady414@gmail.com>
2023-03-23 22:06:06 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,9 +7,9 @@ void more_numbers(void)
 {
 int i;
 int j;
-for (i = 0;i < 11; i++)
+for (i = 0;i < 11; i++);
 {
-for (j = 0;j < 15; j++)
+for (j = 0;j < 15; j++);
 {
 if (j >= 10)
 _putchar(j / 10 + '0');
