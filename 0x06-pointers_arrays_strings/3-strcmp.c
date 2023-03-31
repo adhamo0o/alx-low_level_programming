ee8c6aeff9746558e2e3aafe1a648e509333c523
adhamo0o <adhamelmesady414@gmail.com>
2023-03-30 22:20:31 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,13 +11,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = o;
+int i = 0;
 for (i = 0; s1[i] != 0; i++)
 if (s1[i] > s2[i])
 return ('1');
 else if (s1[i] < s2[i])
 return ('-1');
-else if
+else if (s1[i] = s2[i])
 return (0);
 return (s1[i] - s2[i]);
 }
