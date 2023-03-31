a7734fbf1e66b4e531e213922c8d1df2eda6a13a
adhamo0o <adhamelmesady414@gmail.com>
2023-03-30 17:35:41 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,12 @@
 #include "main.h"
-/**                                                                                              
- **_strncat - concatenates two strings                                                           
- *@dest: Adding to it that src points to                                                         
- *@src: Adding it to dest                                                                        
- *@n: The number of bytes will be added from src to dest                                         
- *Return: a pointer to the resulting string dest                                                 
+
+/**
+ * *_strncat - concatenates two strings.
+ * @dest: Adding to it that src points to.
+ * @src: Adding it to dest.
+ * @n: The number of bytes will be added from src to dest.
+ *
+ * Return: a pointer to the resulting string dest.
  */
 char *_strncat(char *dest, char *src, int n)
 {
