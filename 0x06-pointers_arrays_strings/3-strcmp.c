89731178101866afaad9525faeb377ae7d73f60d
adhamo0o <adhamelmesady414@gmail.com>
2023-03-30 23:16:21 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,24 +16,24 @@ for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 {
 if (s1[i] > s2[i])
 {
-return 1;
+return (1);
 }
 else if (s1[i] < s2[i])
 {
-return -1;
+return (-1);
 }
 }
 if (s1[i] == s2[i])
 {
-return 0;
+return (0);
 }
 else if (s1[i] < s2[i])
 {
-return -1;
+return (-1);
 }
 else
 {
-return 1;
+return (1);
 }
 return (s1[i] - s2[i]);
 }
