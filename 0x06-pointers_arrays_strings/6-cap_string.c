964d6c1d73bbf188f08bc68a8d0446d32ea84d99
adhamo0o <adhamelmesady414@gmail.com>
2023-03-31 00:28:03 -0700

    a

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,14 +18,14 @@ if (capitalize_next && (str[i] >= 'a' && str[i] <= 'z'))
 str[i] -= 32;
 capitalize_next = 0;
 }
-else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || 
-str[i] == ',' || str[i] == ';' || str[i] == '.' || 
-str[i] == '!' || str[i] == '?' ||str[i] == '"' ||
-str[i] == '(' || str[i] == ')' || str[i] == '{' || 
+else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+str[i] == '(' || str[i] == ')' || str[i] == '{' ||
 str[i] == '}')
 {
 capitalize_next = 1;
 }
 }
-return str;
+return (str);
 }
