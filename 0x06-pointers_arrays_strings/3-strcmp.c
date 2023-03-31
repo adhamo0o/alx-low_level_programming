909a605a2aa648315abb22cdd70de07a93254498
adhamo0o <adhamelmesady414@gmail.com>
2023-03-30 23:19:41 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,20 +11,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i;
-for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
-{
-if (s1[i] == s2[i])
-{
-return (0);
-}
-else if (s1[i] < s2[i])
-{
-return (-1);
-}
-else
-{
-return (1);
-}
-return (s1[i] - s2[i]);
+    int i;
+    for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
+        if (s1[i] < s2[i]) {
+            return -1;
+        } else if (s1[i] > s2[i]) {
+            return 1;
+        }
+    }
+    if (s1[i] == s2[i]) {
+        return 0;
+    } else if (s1[i] < s2[i]) {
+        return -1;
+    } else {
+        return 1;
+    }
 }
