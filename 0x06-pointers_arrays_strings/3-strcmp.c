7e34db7b640d398ce4a13e63bd76a9e1c403f8e8
adhamo0o <adhamelmesady414@gmail.com>
2023-03-30 20:56:00 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,14 @@
  * of the first unmatched character
  */
 int _strcmp(char *s1, char *s2)
-int i = 0;
-for (i= 0; s1[i] != 0; i++)
+{
+int i = o;
+for (i = 0; s1[i] != 0; i++)
 if (s1[i] > s2[i])
 return ('1');
 else if (s1[i] < s2[i])
 return ('-1');
 else if
-return ('0');
+return (0);
+return (s1[i] - s2[i]);
 }
