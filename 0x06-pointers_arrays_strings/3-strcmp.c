d23f0f91fba927398418aaffa1fcd40ad060f417
adhamo0o <adhamelmesady414@gmail.com>
2023-03-30 23:27:45 -0700

    a

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _strcmp: a function that compares two strings
+ * _strcmp: compares two strings
  * @s1: A pointer to the first string to be compared
  *  @s2: A pointer to the second string to be compared
  * Return: If str1 < str2, the negative difference of the
