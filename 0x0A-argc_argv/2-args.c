c0f2e896e93014a2e212202112b8f5d2bc5e4865
adhamo0o <adhamelmesady414@gmail.com>
2023-04-10 22:00:36 -0700

    a

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
  *main - prints all arguments it receives
- *@argc: this is the number of argument
- *@argv: this is array of argument
+ *@argc: number of argument
+ *@argv: array of argument
  *Return: 0
  */
 int main(int argc, char *argv[])
 {
 int i;
 for (i = 0; i < argc; i++)
-{
 ptintf("%s\n", argv[i]);
-}
 return (0);
 }
