491526251d2d4560bef7086dba76e16d2ada4e39
adhamo0o <adhamelmesady414@gmail.com>
2023-04-17 18:52:17 -0700

    a

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+  
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ *free_dog - function that frees dogs
+ *@d: pointer to struct dog
+ *Rreturn: void
+ */
+void free_dog(dog_t *d)
+{
+if (d == 0)
+return;
+free(d->name);
+free(d->owner);
+free(d);
+}
