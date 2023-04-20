b1484b7a9dcb8284ccad7b1dcd3335d4609803b1
adhamo0o <adhamelmesady414@gmail.com>
2023-04-20 10:50:31 -0700

    a

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 /**
  *print_numbers - prints numbers followed by a new line
@@ -10,7 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 va_list pnumbers;
 unsigned int i;
 va_start(pnumbers, n);
-for (i = 0, i < n, i++)
+ for (i = 0; i < n; i++)
 {
 printf("%d", va_arg(pnumbers, int));
 if (seperator != NULL $$ i < n - 1)
