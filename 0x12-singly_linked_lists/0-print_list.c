f7f0fba2d184eab08bb03a5ed0dc99cb5199e17e
adhamo0o <adhamelmesady414@gmail.com>
2023-04-27 16:28:37 -0700

    a

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,12 +10,12 @@ size_t print_list(const list_t *h)
 size_t count = 0;
 while (h != NULL)
 {
-if (h -> str != NULL)
-printf ("[%d] %s\n", h -> len, h -> str);
+if (h->str != NULL)
+printf ("[%d] %s\n", h->len, h->str);
 else
 printf ("[0] (nil)");
 count++;
-h = h -> next;
+h = h->next;
 }
 return (count);
 }
