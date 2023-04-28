1cacfd91a04a6b1d466da124261a07f3c44fc412
adhamo0o <adhamelmesady414@gmail.com>
2023-04-27 17:05:30 -0700

    a

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ *list_len -  returns the number of elements in a linked 
+ *@h: singly linked list
+ *Return: number of elements in a linked
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+while (h ! NULL)
+{
+h = h->next;
+count++;
+}
+return (count);
+}
