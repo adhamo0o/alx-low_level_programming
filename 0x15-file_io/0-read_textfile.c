2edf14723ca85f5a5d0f4f08a014a8f6152f48aa
adhamo0o <adhamelmesady414@gmail.com>
2023-05-09 17:28:16 -0700

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,17 +13,17 @@ size_t x, y;
 char *buffer;
 if (filename == NULL)
 return (0);
-fd = open(filename, 0_RDONLY);
+fd = open(filename, O_RDONLY);
 if (fd < 0)
 return (0);
-buffer = malloc(sizeof(char) * letters));
+buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
 {
 close(fd);
 return (0);
 }
 x = read(fd, buffer, letters);
-y = write(STDOUT_FILENO, buffer, br);
+y = write(STDOUT_FILENO, buffer, x);
 if (x != y)
 {
 free(buffer);
