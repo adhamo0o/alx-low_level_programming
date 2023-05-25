7bc3781c58ccf26bd17d1c99eeda1d753d0b6748
adhamo0o <adhamelmesady414@gmail.com>
2023-05-25 04:14:50 -0700

    a

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,34 @@
+#include <elf.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main -  a program that displays the
+ *  information contained in the ELF header at the start of an ELF file.
+ * @ac: arguments count
+ * @av: arguments vactor
+ * Return: 0 on success
+ */
+int main(int argc, char **argv)
+{
+	int fd;
+
+	if (argc != 2)
+	{
+		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31);
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		write(STDERR_FILENO, "Error: Can't read from file\n", 28);
+		exit(98);
+	}
+	return (0);
+}
