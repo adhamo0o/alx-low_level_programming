d2fb61c5fed316834f9515b76109d7a6c327e2e8
adhamo0o <adhamelmesady414@gmail.com>
2023-06-07 19:20:48 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,9 +8,10 @@
  */
 int is_prime_number(int n)
 {
-if (is_prime_number(n % 2 == 0 || n % 3 == 0))
+if (n == 1)
 return (0);
-else if (n <= 0)
+n = 4;
+if (is_prime_number(n % 2 == 0 || n % 3 == 0))
 return (0);
 else
 return (1);
