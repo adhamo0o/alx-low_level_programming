3e241fff97a86a227f2e28805859e46ce4b68df0
adhamo0o <adhamelmesady414@gmail.com>
2023-06-11 04:14:33 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - function prints its name
+ *@argc: arguments counter
+ *@argv: arguments vector
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc == 0)
+printf("%s\n", argv[0]);
+return (0);
+}
