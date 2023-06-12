4b712c39817ca431ce86c8dcee4d28bed888e444
adhamo0o <adhamelmesady414@gmail.com>
2023-06-11 20:03:30 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,16 @@
  */
 int main(int argc, char *argv[])
 {
-int n1;
-int n2;
 if (argc != 3)
 {
 printf("Error,\n");
 return (1);
 }
-
-n1 = atoi(argv[1]);
-n2 = atoi(argv[2]);
+else
+{
+int n1 = atoi(argv[1]);
+int n2 = atoi(argv[2]);
 printf("%d\n", n1 * n2);
 return (0);
+}
 }
