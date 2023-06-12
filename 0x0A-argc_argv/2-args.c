38bd2b6f5deca7737a7e12b2411305ade5b3ebe5
adhamo0o <adhamelmesady414@gmail.com>
2023-06-11 18:09:28 -0700

    a

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,11 +9,13 @@
 int main(int argc, char *argv[])
 {
 int i;
-for (i = 0; (i = argc); i++)
-printf("%s\n", argv[i - 1]);
+while ((i = argc))
+{
+printf("%s\n", argv[i]);
+argc++;
+}
+
+
 
 return (0);
-
-
-
 }
