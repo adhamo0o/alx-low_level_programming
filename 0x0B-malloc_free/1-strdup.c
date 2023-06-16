b8a23fd547e3748c7571f66027cd521b2d86030b
adhamo0o <adhamelmesady414@gmail.com>
2023-06-16 07:28:01 -0700

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,21 +4,24 @@
 /**
 *_stdrup - function that returns a pointer to a newly allocated space
 *@str: pointer ro char
-*
 *Return: NULL if str = NULL
-*
 */
 char *_strdup(char *str)
 {
 unsigned int x;
 char *ptr;
+
 if (str == NULL)
 return (NULL);
+
 ptr = (char *)malloc(sizeof(char));
 if (ptr == NULL)
 return (NULL);
+
 for (x = 0; x < strlen(str + 1); x++)
 ptr[x] = str[x];
+
+putchar('\0');
 return (ptr);
 
 free(ptr);
