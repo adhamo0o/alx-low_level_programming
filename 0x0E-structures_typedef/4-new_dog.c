7358075b0c8ad6c53bc1e56f399c6e9f6496bb32
adhamo0o <adhamelmesady414@gmail.com>
2023-06-20 15:58:37 -0700

    a

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,15 +12,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+int len1, len2;
+
+len1 = strlen(name);
+len2 = strlen(owner);
+
 dog_t *new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 {
 return (NULL);
-free(dog_t);
 }
 
+new_dog->name = malloc(sizeof(char) * (len1 + 1));
+new_dog->owner = malloc(sizeof(char) *(len2 + 1));
+if (new_dog->name == NULL || new_dog->owner == NULL)
+{
+free(new_dog->name);
+free(new_dog->owner);
+free(new_dog);
+return NULL;
+}
+
+ 
 new_dog->name = strdup(name);
 new_dog->age = age;
 new_dog->owner = strdup(owner);
-return (new_dog);
+return (new_dog):
+free(new_dog);
 }
