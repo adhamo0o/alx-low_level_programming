fbc0f1c8917e0e59899d17839daf38e8c0171a63
adhamo0o <adhamelmesady414@gmail.com>
2023-06-20 05:16:13 -0700

    a

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,7 +20,7 @@ printf("Name: %s\n", d->name);
 printf("Age: %.1f\n", d->age);
 
 if (d->owner == NULL)
-printf("Owner: (nill)\n");
+printf("Owner: (nil)\n");
 else
 printf("Owner: %s\n", d->owner);
 
