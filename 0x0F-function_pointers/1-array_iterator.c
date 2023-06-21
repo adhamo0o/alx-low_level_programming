a998194644e0da5f2503551daa937b592e94d66c
adhamo0o <adhamelmesady414@gmail.com>
2023-06-21 09:56:52 -0700

    a

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#inclue "function_pointer.h"
+/**
+ *aray+iterator - function that executes a function given as a parameter
+ *@array: pointer to int
+ *@size: size of the array
+ *@action: pointer to the function
+ *
+ *Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+
+}
