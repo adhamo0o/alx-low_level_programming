04160a6f0567725d5ae4f211a09cbcd33af51eac
adhamo0o <adhamelmesady414@gmail.com>
2023-06-21 23:58:19 -0700

    a

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "finction_pointers.h"
+/**
+ *get_op_func - pointer to fun
+ *@s: pointer to char
+ *
+ *Return: op
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
+
+while (ops[i].op != NULL)
+{
+if (*(ops[i].op) == *s)
+return (ops[i].f);
+}
+return (NULL);
+}
