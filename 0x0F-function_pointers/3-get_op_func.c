23782c9ab73e2085e9be2ef5065327a735ff5883
adhamo0o <adhamelmesady414@gmail.com>
2023-06-22 01:05:51 -0700

    k

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,6 +10,8 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
+int i = 0;
+
 op_t ops[] = {
 {"+", op_add},
 {"-", op_sub},
@@ -18,7 +20,6 @@ op_t ops[] = {
 {"%", op_mod},
 {NULL, NULL}
 };
-int i = 0;
 
 while (ops[i].op != NULL)
 {
