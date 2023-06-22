12ddd3be68b688ff1eb7c1a0b0deac165aafd0b7
adhamo0o <adhamelmesady414@gmail.com>
2023-06-21 19:48:55 -0700

    a

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,11 +12,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 int x;
 if (size <= 0)
 return (-1);
-
+if (array != NULL && cmp != NULL)
+{
 for (x = 0; x < size; x++)
 {
 if (cmp(array[x]) != 0)
 return (x);
+}
 }
 return (-1);
 
