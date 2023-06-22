b3b0613e4f4503a743b2befcab07479deb875f47
adhamo0o <adhamelmesady414@gmail.com>
2023-06-22 00:42:29 -0700

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,11 +1,34 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  *main - the main function
- *
- *
- *
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: int
  */
-int main()
+int main(int argc, char *argv[])
 {
+int result, num1, num2;
+int (*fun)(int, int);
 
+if (argc != 4)
+printf("Error\n");
+exit(98);
+
+fun = get_op_func(argv[2]);
+if (fun == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+ 
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+
+result = fun(num1, num2)
+printf("%d\n", result);
+
+return (0);
 
 }
