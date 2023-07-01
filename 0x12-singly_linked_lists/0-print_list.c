adbde139df1ea4a6dc852d217209518ba011334b
adhamo0o <adhamelmesady414@gmail.com>
2023-07-01 09:18:45 -0700

    first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_list - fun prints all the elements
+ * @h: pointer to list
+ *
+ * Return: numbers of elements
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *ptr;
+	unsigned int count = 0;
+
+	ptr = h;
+	while (ptr)
+	{
+		if (ptr->str != NULL)
+			printf("[%d] %s\n", count, ptr->str);
+		else
+			printf("[0] (nil)\n");
+	count++;
+	ptr = ptr->next;
+	}
+	return (count);
+}
