43b82ed829eb161cca4929c4e19db81a2ccd007a
adhamo0o <adhamelmesady414@gmail.com>
2023-07-01 15:31:24 -0700

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -6,5 +6,14 @@
  */
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *current = head;
+	list_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
 }
