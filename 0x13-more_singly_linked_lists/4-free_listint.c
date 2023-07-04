c26ef08b4915f4d7b22a9f3a3299011b5f1a10ee
adhamo0o <adhamelmesady414@gmail.com>
2023-07-04 15:47:05 -0700

    five

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,13 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *ptr = *head;
+	listint_t *ptr = head;
 	listint_t *next;
 
 	while (ptr != NULL)
 	{
 		next = ptr->next;
-		free(ptr->n);
 		free(ptr);
 		ptr = next;
 	}
