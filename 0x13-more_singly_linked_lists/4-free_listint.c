25562b52ce9c4406d8b1c9be2d2ffa27ce706a1d
adhamo0o <adhamelmesady414@gmail.com>
2023-07-04 15:40:15 -0700

    five

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint - function free the list
+ * @head: pointer to the first element
+ * Return: 0
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr = *head;
+	listint_t *next;
+
+	while(ptr->next != NULL)
+	{
+		next = ptr->next;
+		free(ptr->n);
+		free(ptr);
+		ptr = next;
+	}
+}
