81bffbbba93da9ead1444185d27538c233d04564
adhamo0o <adhamelmesady414@gmail.com>
2023-07-04 08:12:27 -0700

    two

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * listint_len - function that returns the number of elements
+ * @h: pointer
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *ptr;
+	unsigned int count = 0;
+
+	ptr = h;
+	while (ptr)
+	{
+		count++
+		ptr = ptr->next;
+	}
+	return (count);
+}
