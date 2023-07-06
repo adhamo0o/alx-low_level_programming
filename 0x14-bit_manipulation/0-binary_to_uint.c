13a479f4f04e6688ca132dd7af4029a2d7e7d6c3
adhamo0o <adhamelmesady414@gmail.com>
2023-07-06 11:24:12 -0700

    1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * binary_to_unit -  function convert binary to unsigned int
+ * binary_to_uint - function convert binary to unsigned int
  * @b: pointer to string
  * Return: the converted number
  */
