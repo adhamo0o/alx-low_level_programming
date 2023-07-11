9c42cfa46a187160f069f09626801240a21340ed
adhamo0o <adhamelmesady414@gmail.com>
2023-07-11 11:02:59 -0700

    2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,25 +2,26 @@
 /**
  * create_file - function that creates a file.
  * @filename: the name of the file to create
- * @text_content  is a NULL terminated string to write to the file
+ * @text_content: is a NULL terminated string to write to the file
  * Return: 1 on success
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t w;
+	size_t w;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	fd = open(filename, 'w');
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (fd < 0)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
-		w = (fd, text_conent, strlen(text_content));
+		w = write(fd, text_content, strlen(text_content));
+
 		if (w != strlen(text_content))
 		{
 			close(fd);
